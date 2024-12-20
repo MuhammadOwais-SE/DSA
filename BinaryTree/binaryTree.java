ad84c53977c59e6642e90514471e71764b7bd6dc
CoWeDeveloper <owaisilyas22@gmail.com>
2024-12-20 17:47:39 +0500

    postOder function has been added.

--- BinaryTree/binaryTree.java
+++ BinaryTree/binaryTree.java
@@ -22,13 +22,45 @@ public class binaryTree{
       newNode.right = buildTree(node);
       return newNode;
    }
+ 
+   public static void preOrder(Node root){
+      if(root == null){
+         return;
+      }
+      System.out.print(root.data + " ");
+      preOrder(root.left);
+      preOrder(root.right);
+   }
+   public static void inOrder(Node root){
+      if(root == null){
+         return;
+      }
+      preOrder(root.left);
+      System.out.print(root.data + " ");
+      preOrder(root.right);
+   }
+   public static void postOrder(Node root){
+      if(root == null){
+         return;
+      }
+      postOrder(root.left);
+      postOrder(root.right);
+      System.out.print(root.data + " ");
+   }
+      
 }
+
 
 public static void main(String arg[]){
    int nodes [] = {1,2,4,-1,-1,5,-1,-1,3, -1,6,-1,-1};
    BinaryTree tree = new BinaryTree();
    Node root = tree.buildTree(nodes);
-   System.out.print(root.data);
+   tree.preOrder(root);
+   System.out.println(" ");
+   tree.inOrder(root);
+   System.out.println(" ");
+   tree.postOrder(root);
+   System.out.println(root.data);
 }
 
 }
