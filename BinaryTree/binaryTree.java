265eafceb3f26b066aa8ec214c9095fc77a1975d
CoWeDeveloper <owaisilyas22@gmail.com>
2024-12-26 20:53:24 +0500

    tree diameter question is solved.

--- BinaryTree/binaryTree.java
+++ BinaryTree/binaryTree.java
@@ -88,21 +88,51 @@ public class binaryTree{
       int rh = treeHeight(root.right);
       return Math.max(lh, rh) + 1;
    }
+
+   public static int treeDiameter(Node root){
+      if(root == null){
+         return 0;
+      }
+
+      int lh = treeHeight(root.left);
+      int leftDiameter = treeDiameter(root.left); 
+      int rh = treeHeight(root.right);
+      int rightDiameter = treeDiameter(root.right);
+      int selfDiameter = lh + rh + 1;
+
+      return Math.max(selfDiameter,Math.max(leftDiameter, rightDiameter))
+   }
 }
 
 
+/**
+ * @param arg
+ */
 public static void main(String arg[]){
    int nodes [] = {1,2,4,-1,-1,5,-1,-1,3, -1,6,-1,-1};
    BinaryTree tree = new BinaryTree();
-   Node root = tree.buildTree(nodes);
+   // Node root = tree.buildTree(nodes);
    // tree.preOrder(root);
    // System.out.println(" ");
    // tree.inOrder(root);
    // System.out.println(" ");
    // tree.postOrder(root);
    // System.out.println(" ");
-   tree.levelOrder(root);
+   // tree.levelOrder(root);
    // System.out.println(root.data);
+
+
+   // for height and treeDiamter
+   // Node root = new Node(1);
+   // root.left = new Node(2);
+   // root.right = new Node(3);
+   // root.left.left = new Node(4);
+   // root.left.right = new Node(5);
+   // root.right.left = new Node(6);
+   // root.right.right = new Node(7);
+
+
+   System.out.print(treeDiameter(root));
 }
 
 }
